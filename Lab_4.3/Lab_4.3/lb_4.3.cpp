c3de2e714114786bb132c436759200f272ef9460
ANDRII CHUKHRAI <andrii.chukhrai.ik.2024@lpnu.ua>
2024-09-23 17:52:13 +0300

    Add files via upload

---
+++ Lab_4.3/Lab_4.3/lb_4.3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, F, a, b, c;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << fixed;
+	cout << "----------------------" << endl;
+	cout << "|" << setw(5) << "x" << "   |"
+		<< setw(7) << "F" << "    |" << endl;
+	cout << "----------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x < 5 && b != 0)
+			F = a * (x + 7) * (x + 7) - b;
+		else
+			if (x > 0 && b == 0)
+				F = (x - c * a) / a * x;
+			else
+				F = x / c;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << F
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "----------------------" << endl;
+	return 0;
+}
